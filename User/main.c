368716501de56cdb8a2fd5161dc5249bfee4ef04
Azazel <384735481@qq.com>
2026-03-31 21:50:40 +0800

    [fix] remove obj & listing

--- User/main.c
+++ User/main.c
@@ -1,9 +1,12 @@
 #include "stm32f10x.h"
 #include "Serial.h"
+#include "OLED.h"
 
 int main(){
-	
 	Serial_Init();
+	OLED_Init();
+	
+	OLED_ShowString(1,1,"Hello World");
 	Serial_SendString("你好 我的爱人");
 	while(1){
 		
